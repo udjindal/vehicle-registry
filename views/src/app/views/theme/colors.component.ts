e4c3863a840db7dbdf0bf0e594099baa4995beb2
Kashish Jain <jainkashish96@gmail.com>
2019-04-30 01:48:23 +0530

    added existing user functionality

--- views/src/app/views/theme/colors.component.ts
+++ views/src/app/views/theme/colors.component.ts
@@ -1,14 +1,48 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { getStyle, rgbToHex } from '@coreui/coreui/dist/js/coreui-utilities';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'environments/environment';
+import { routerNgProbeToken } from '@angular/router/src/router_module';
+import { routes } from '../../app.routing';
 
 @Component({
   templateUrl: 'colors.component.html'
 })
 export class ColorsComponent {
-  constructor(@Inject(DOCUMENT) private _document: any) {}
 
   existing: boolean = true
+
+  selected_owner = "choose owner";
+  vehicle_type = "TwoWheeler";
+  api_path = environment.api_path;
+  owners;
+  number : string = ""
+
+  constructor(private httpClient: HttpClient){
+    this.httpClient.get(this.api_path+'owner').subscribe((res)=>{
+
+      if(res.success === true){
+        console.log(res);
+        this.owners=res.owners;
+      }
+      else{
+        window.alert("unable to connect to backend!")
+        window.location.reload();
+      }
+        
+    })
+  }
+
+  change_vehicle_type(selected){
+    this.vehicle_type=selected;
+    console.log(selected)
+  }
+
+  change_owner(selected){
+    this.selected_owner=selected;
+    console.log(selected)
+  }
 
   existing_person(event){
     this.existing = true;
@@ -18,5 +52,35 @@ export class ColorsComponent {
   new_person(event){
     this.existing = false;
     //alert("New");
+  }
+
+  add_existing(event){
+    console.log(this.number);
+    if(this.number === ""){
+      window.alert("Enter Vehicle Number!");
+    }
+
+    else if(this.selected_owner === "choose owner"){
+      window.alert("Select Owner");
+    }
+
+    else{
+      var data = {owner_id : this.selected_owner, category : this.vehicle_type, number : this.number};
+
+      this.httpClient.post(this.api_path+'vehicle',data).subscribe((res)=>{
+
+        console.log(res.success); 
+        if(res.success === true){
+          window.alert("New Entry Added");
+          this.name = ""
+
+        }
+        else{
+          window.alert("error");
+        }    
+      
+      })
+    }
+
   }
 }
--- views/src/app/views/theme/theme.module.ts
+++ views/src/app/views/theme/theme.module.ts
@@ -1,6 +1,7 @@
 // Angular
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { ColorsComponent } from './colors.component';
 import { TypographyComponent } from './typography.component';
@@ -8,10 +9,15 @@ import { TypographyComponent } from './typography.component';
 // Theme Routing
 import { ThemeRoutingModule } from './theme-routing.module';
 
+import { HttpClientModule } from '@angular/common/http';
+import {environment} from 'environments/environment';
+
 @NgModule({
   imports: [
+    FormsModule,
     CommonModule,
-    ThemeRoutingModule
+    ThemeRoutingModule,
+    HttpClientModule
   ],
   declarations: [
     ColorsComponent,
