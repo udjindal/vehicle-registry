668884424e1cec2e098cae94c66b5a2c08cf8d62
Kashish Jain <jainkashish96@gmail.com>
2019-04-30 05:06:04 +0530

    fixed adding user

--- views/src/app/views/theme/colors.component.ts
+++ views/src/app/views/theme/colors.component.ts
@@ -114,16 +114,18 @@ export class ColorsComponent {
     }
 
     else{
-      var data = {name : this.name.toLowerCase, category : this.owner_type, mobile : this.phone_number};
+      var data = {name : this.name.toLowerCase(), category : this.owner_type, number : this.phone_number};
+
+      console.log(data);
 
       this.httpClient.post(this.api_path+'owner',data).subscribe((res : any)=>{
 
         console.log(res.success); 
         if(res.success === true){
-          var data1 = {mobile : this.phone_number};
+          var data1 = {number : this.phone_number};
           this.httpClient.get(this.api_path+'owner/'+this.phone_number).subscribe((res:any)=>{
             if(res.success === true){
-              var id = res._id;
+              var id = res.owner._id;
               var data = {owner_id : id, category : this.vehicle_type, number : this.number.toLowerCase()};
 
               this.httpClient.post(this.api_path+'vehicle',data).subscribe((res : any)=>{
@@ -132,6 +134,7 @@ export class ColorsComponent {
                 if(res.success === true){
                   window.alert("New Entry Added");
                   this.number = "";
+                  window.location.reload();
                 }
                 else{
                   window.alert("error3");
